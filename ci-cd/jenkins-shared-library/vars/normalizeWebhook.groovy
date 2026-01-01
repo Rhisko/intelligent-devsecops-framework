552541d6608b25e4a322f5151d68a52bef48922e
Risko <risko.alexander@gmail.com>
2026-01-01 21:45:21 +0700

    Deploy library

--- ci-cd/jenkins-shared-library/vars/normalizeWebhook.groovy
+++ ci-cd/jenkins-shared-library/vars/normalizeWebhook.groovy
@@ -12,7 +12,7 @@ def call(Map envVars) {
 
     // Git refs
     ctx.ref     = envVars.REF
-    ctx.baseRef = envVars.BASE_REF.replace('refs/heads/', '')
+    ctx.baseRef = envVars.BASE_REF ? envVars.BASE_REF.replace('refs/heads/', '') : ''
 
     // Commit
     ctx.commitHashBefore = envVars.COMMIT_HASH_BEFORE
