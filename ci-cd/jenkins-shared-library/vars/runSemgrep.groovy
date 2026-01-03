911d471b01b6eda16364d486f5274d86d5c366b2
Risko <risko.alexander@gmail.com>
2026-01-03 13:01:28 +0700

    Deploy library linter tools ruff

--- ci-cd/jenkins-shared-library/vars/runSemgrep.groovy
+++ ci-cd/jenkins-shared-library/vars/runSemgrep.groovy
@@ -61,7 +61,7 @@ def call(Map config = [:]) {
     runner.run(
         workDir,
         image,
-        "${command} --output ${outputFile} || true",
+        "${command} --output semgrep.json || true",
         [:],  // env
         []    // extra volumes
     )
