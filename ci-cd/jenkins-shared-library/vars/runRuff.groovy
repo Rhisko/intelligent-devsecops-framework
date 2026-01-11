359e3492618e5807baa4f0d2ae98abaf2bd4144a
Risko <risko.alexander@gmail.com>
2026-01-11 18:46:02 +0700

    Added library for sonarscanner

--- ci-cd/jenkins-shared-library/vars/runRuff.groovy
+++ ci-cd/jenkins-shared-library/vars/runRuff.groovy
@@ -4,14 +4,14 @@ def call(Map config = [:]) {
     def meta      = cfgLoader.load("tool-metadata").ruff
 
     def target = config.target ?: meta.defaults.target
-    // def select = config.select ?: meta.defaults.select
+    def select = config.select ?: meta.defaults.select
     def format = config.output_format ?: meta.defaults.output_format
 
     // println "[DEBUG][runRuff] target=${target}, select=${select}, format=${format}"
 
     def command = meta.command
         .replace('{target}', target)
-        // .replace('{select}', select)
+        .replace('{select}', select)
         .replace('{output_format}', format)
 
     def workDir = "/ci-workspace/ruff/${env.JOB_NAME}-${env.BUILD_NUMBER}"
@@ -21,9 +21,7 @@ def call(Map config = [:]) {
       mkdir -p "${workDir}" && \
       cp -R . "${workDir}/"
     """
-// def commandWithOutput = """
-// sh -c "${command} > ruff.json || true"
-// """.trim()
+
 
     def runner = new devsecops.DockerRunner(this)
     def outputFile = "${workDir}/ruff.json"
